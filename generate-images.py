c160a80a6f84eb043de6fa519dd007cbff06c44f
Javier Urrutia <javier.urrutia.r@ug.uchile.cl>
2019-12-06 11:47:31 -0300

    made asynchronous generation of images

--- generate-images.py
+++ generate-images.py
@@ -3,7 +3,7 @@
 import os
 import glob
 import cv2 as cv
-
+import asyncio
 
 def save_img(dname, fn, i, frame):
 	cv.imwrite('{}/{}_{}_{}.png'.format(
@@ -14,7 +14,7 @@ out_dir = 'images'
 if not os.path.exists(out_dir):
 	os.makedirs(out_dir)
 
-def convert(dir):
+async def convert(dir):
 	for dname in sorted(glob.glob(dir)):
 		for fn in sorted(glob.glob('{}/*.seq'.format(dname))):
 			cap = cv.VideoCapture(fn)
@@ -27,5 +27,13 @@ def convert(dir):
 				i += 1
 			print(fn)
 
-convert('../caltech/train*')
-convert('../caltech/test*')
+async def main():
+	task1 = asyncio.create_task(
+		convert('../caltech/train*'))
+	task2 = asyncio.create_task(
+		convert('../caltech/test*'))
+
+	await task1
+	await task2
+
+asyncio.run(main())
